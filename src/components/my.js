7282f06a314a11f77a8dfb05815ee237b6e4d122
artijan <hunbari123@gmail.com>
2022-07-05 12:31:00 +0900

    add sphere mesh in home
    addddd

--- src/components/my.js
+++ src/components/my.js
@@ -46,8 +46,8 @@ function My() {
             color="#A53CB9"
             position={[-1, 0, 0]}
             lineHeight="1"
-            fontSize="8"
-          >{`HELLO WORLD!`}</Caption>
+            fontSize="12"
+          >{`HI THERE!`}</Caption>
 
           <AniSphere
             color="#6254C6"
@@ -59,7 +59,7 @@ function My() {
             roughness={0.5}
           />
           <AniSphere
-            color="#4F9593"
+            color="#76954F"
             args={[1, 100, 100]}
             scale="0.3"
             position={[3, 2, -8]}
@@ -77,10 +77,37 @@ function My() {
             roughness={0.8}
           />
           <AniSphere
+            color="#721ABB"
+            args={[1, 100, 100]}
+            scale="1"
+            position={[-1, 3, -20]}
+            distort={0.6}
+            speed={1}
+            roughness={0.8}
+          />
+          <AniSphere
+            color="#1A96BB"
+            args={[2, 100, 100]}
+            scale="1.5"
+            position={[9, 5, -20]}
+            distort={0.4}
+            speed={2}
+            roughness={0.8}
+          />
+          <AniSphere
             color="#7E61DF"
             args={[1, 100, 100]}
             scale="0.2"
             position={[-2, 0, -1]}
+            distort={0.3}
+            speed={6}
+            roughness={0.3}
+          />
+          <AniSphere
+            color="#7E61DF"
+            args={[1, 100, 100]}
+            scale="0.5"
+            position={[-3, -1, -1]}
             distort={0.3}
             speed={6}
             roughness={0.3}
