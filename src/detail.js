c4644a59d7efaa8234aa8308cdafb54e5b6dda5a
artijan <hunbari123@gmail.com>
2022-08-22 08:02:03 +0900

    making detail page
    wip

--- src/detail.js
+++ src/detail.js
@@ -7,7 +7,11 @@ function Detail() {
         <div className="text-main">
           <div className="main">상세이미지 디자인</div>
           <div className="sub">
-            정기업체 수금/관리, 주요 제품 상세이미지 제작
+            컨텐츠 계약업체 관리,
+            <br />
+            제품 입/출고 관리 및 촬영,
+            <br />
+            컴퓨터 카테고리 제품 상세이미지 제작
           </div>
         </div>
         <div className="image">image1</div>
