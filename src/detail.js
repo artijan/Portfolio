dc21c46aa68764efd0b45bc4417181a8110390f9
artijan <hunbari123@gmail.com>
2022-08-23 08:06:22 +0900

    develop page1 design
    wip

--- src/detail.js
+++ src/detail.js
@@ -14,7 +14,16 @@ function Detail() {
             컴퓨터 카테고리 제품 상세이미지 제작
           </div>
         </div>
-        <div className="image">image1</div>
+        <div className="image">
+          <img src="images/page1_brief.jpg" alt="" />
+          <div className="image-text">
+            컨텐츠 계약 업체 관리/커뮤니케이션을 포함하여, 상세이미지 제작에
+            필요한 실 제품의 입/출고 관리를 진행하였습니다.
+            <br />
+            입고된 실 제품은 자체 스튜디오에서 촬영을 진행하였으며, 촬영된
+            이미지로 상세이미지를 제작하였습니다.
+          </div>
+        </div>
         <div className="image">image2</div>
       </div>
     </>
